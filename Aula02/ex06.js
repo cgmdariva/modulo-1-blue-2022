eceb7145b7b061cc02f7385e6865a28b6fe13e2c
Caio Giovani Malavazi Dariva <cgmdariva@outlook.com>
2022-06-28 23:17:43 -0300

    Create ex06.js
    starting exercise 6

---
+++ Aula02/ex06.js
@@ -0,0 +1,11 @@
+// 6) Calculadora de Dano - Escreva um programa que receba dois valores digitados pelo usuário: quantidade de vida de um monstro
+// (entre 10 e 50) e valor do ataque do jogador por turno (entre 5 e 10). Baseado nos valores digitados, exiba a quantidade de
+// turnos que o jogador irá demorar para conseguir derrotar o monstro (o jogador irá derrotar o monstro em 8 turnos).
+
+// const prompt = require("prompt-sync")();
+
+// monsterLife = prompt("How many points of health the monster have? (between 10 and 50)");
+// heroAttack = prompt("How many points of attack the hero have? (between 5 and 10)");
+
+// turnsInt = parseInt(monsterLife / heroAttack);
+// turnsTotal =
